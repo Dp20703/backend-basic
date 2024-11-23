99ace386c703f487bffda5626fcedeebafcbf286
dp <codewithdp2073@gmail.com>
2024-11-23 17:32:15 +0530

    middleware

--- express.js
+++ express.js
@@ -1,9 +1,26 @@
 const express = require('express')
-const app = express()
+const morgan = require('morgan')//Third-party middleware:
+const app = express()//to use express you need to first call the express:
 
+//view engine:ejs for rendering html pages
 app.set('view engine', 'ejs')
-app.get('/', (req, res) => {
-    res.render('index')
+
+//middleware:
+//Third-party middleware:
+app.use(morgan('dev'))
+//Custom middleware:
+app.use((req, res, next) => {
+    console.log("This is middleware")
+    // res.send('this is middleware')
+    return next()
+})
+
+//routes:
+app.get('/', (req, res, next) => {
+    console.log("home page middleware")
+    next()
+}, (req, res) => {
+    res.render('index')//rendering html page index
 })
 app.get('/about', (req, res) => {
     res.send('About Page')
