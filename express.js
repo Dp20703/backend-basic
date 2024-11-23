3af98a4b44a42efaa795794aa39bfffec6aba412
dp <codewithdp2073@gmail.com>
2024-11-23 17:03:50 +0530

    view engine ejs

--- express.js
+++ express.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
-    res.send('Home Page')
+    res.render('index')
 })
 app.get('/about', (req, res) => {
     res.send('About Page')
