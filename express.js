07e9669751dcf99b54da6e5047a652c9749b27da
dp <codewithdp2073@gmail.com>
2024-12-04 21:58:13 +0530

    link css and js files

--- express.js
+++ express.js
@@ -19,6 +19,7 @@ app.use((req, res, next) => {
 //used for read the body of request in POST method:
 app.use(express.json())
 app.use(express.urlencoded({ extends: true }))
+app.use(express.static("public"))
 
 //routes:
 app.get('/', (req, res, next) => {
